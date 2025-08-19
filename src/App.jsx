847c9403dc7ffa373137940fccb75e0a442b6a50
Rudra805 <rudramishra9949@gmail.com>
2025-08-19 23:20:28 +0530

    context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './context';
 
@@ -8,14 +8,48 @@ function App() {
   //creating methods
 
   const addTodo=(todo)=>{
-     setTodos((previos)=>[{id:Date.now(),...todo},...previos])     //previos array me new value add kari ... spreade kiya
+     setTodos((previos)=>[{id:Date.now(),...todo},...previos])    //previos array me new value add kari ... spreade kiya
+  }
+  const updateTodo=(id,todo)=>{
+    setTodos((previos)=>previos.map((item)=>
+      // {previosTodo.id ===id ? todo  : previosTodo}
+    {
+      if(item.id!==id) 
+        return todo
+      
+    }
+    ))
 
+  }
+  const deleteTodo=(id)=>{  //means we create a new array in which expact the value that i want to delte all values are awilble
+    setTodos((prev)=>prev.filter((item)=>{
+    if(item.id!==id){
+       return item
+    }
+    
+   }))
+  }
+  const toggle=(id)=>{
+    setTodos((prev)=>prev.map((item)=>item===id?{...item , completed:!item.completed}:item))
   }
 
 
+  useEffect(() => {
+   // localStorage.getItem("todos")=>this give values in string so we use JSoN.parse
+  const todos= JSON.parse(localStorage.getItem("todos"))    //getdata
+
+  if(todos&&todos.length>0){
+    setTodos(todos);
+  }
+  }, [])
+  useEffect(()=>{
+localStorage.setItem("todos",JSON.stringify(todos));
+  },[todos])
+  
+
   return (
    <>
-   <TodoProvider value={{todos,addTodo,deleteTodo,updateTodo}}>
+   <TodoProvider value={{todos,addTodo,deleteTodo,updateTodo,toggle}}>
 
     <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
